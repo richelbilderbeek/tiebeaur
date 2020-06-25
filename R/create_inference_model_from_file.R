19cbbdd75941ebe194cb51bfaf0fa414aa9775ab
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-25 13:46:42 +0200

    Stub of 'create_inference_model_from_file'

---
+++ R/create_inference_model_from_file.R
@@ -0,0 +1,19 @@
+#' Create an inference model from file
+#' @param xml_filename name of a BEAST2 XML input file,
+#'   as can be created by BEAUti2 or
+#'   using \link[beautier]{create_beast2_input_file_from_model}
+#' @export
+create_inference_model_from_file <- function(xml_filename) {
+  inference_model <- beautier::create_test_inference_model()
+  readLines(xml_filename)
+  xml <- xml2::read_xml(xml_filename)
+  xml_run <- xml2::xml_child(xml, "run")
+  xml_run_distr <- xml2::xml_child(xml_run, "distribution")
+  xml_run_distr_distr <- xml2::xml_child(xml_run_distr, "distribution")
+  xml_run_distr_distr_distr <- xml2::xml_child(xml_run_distr_distr, "distribution")
+  spec <- xml2::xml_attr(xml_run_distr_distr_distr, "spec")
+  if (spec == "beast.evolution.speciation.YuleModel") {
+    inference_model$tree_prior <- beautier::create_yule_tree_prior()
+  }
+  inference_model
+}
--- tests/testthat/test-create_inference_model_from_file.R
+++ tests/testthat/test-create_inference_model_from_file.R
@@ -1,6 +1,5 @@
 test_that("use", {
 
-  skip("WIP")
   beast2_filename <- tempfile()
   inference_model <- beautier::create_test_inference_model()
   beautier::create_beast2_input_file_from_model(
@@ -9,6 +8,12 @@ test_that("use", {
     inference_model = inference_model
   )
 
-  inference_model_out <- create_inference_model_from_file(beast2_filename)
+  inference_model_out <- create_inference_model_from_file(
+    xml_filename = beast2_filename
+  )
   expect_silent(beautier::check_inference_model(inference_model_out))
+  expect_equal(
+    inference_model$tree_prior$name,
+    inference_model_out$tree_prior$name
+  )
 })
